d01878181aecf55367e7487ba3cc72fb73be7197
rangeset <2432647223@qq.com>
2026-07-07 19:26:12 +0800

    fix(pack): remove the newline
    remove the newline where in the GPG_KEY
    and changed the GPG_KEY transmit from --build-arg to --secret

--- pack/expack/linux/fedora/pkg.sh
+++ pack/expack/linux/fedora/pkg.sh
@@ -7,6 +7,3 @@ make -S . -B build \
 
 cmake --build build --config Release --target install
 cpack -C Release -G RPM
-echo "$GPG_KEY" | gpg --import
-KEY_ID=$(gpg --list-secret-keys --keyid-format LONG | awk '/sec/{print $2}' | cut -d'/' -f2)
-rpm --addsign --define "_gpg_name $KEY_ID" $OUT_PATH/*.rpm
